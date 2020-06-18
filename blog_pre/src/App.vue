10253498a4a8f0e0d28bbc10c6058a2c5943667f
chj <470945645@qq.com>
2020-06-18 15:36:25 +0800

    欢迎页面

--- blog_pre/src/App.vue
+++ blog_pre/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
-    
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view />
+    </transition>
   </div>
 </template>
+
+ <script>
+export default {
+  name: "App",
+  data() {
+    return {
+      transitionName: ""
+    };
+  },
+  watch: {
+    // 使用watch 监听$router的变化
+    $route(to, from) {
+      // 如果to索引大于from索引,判断为前进状态,反之则为后退状态
+      if (to.meta.index > from.meta.index) {
+        // 设置动画名称
+        this.transitionName = "slide-enter";
+      } else {
+        this.transitionName = "slide-leave";
+      }
+    }
+  }
+};
+</script>
 
 <style lang="less">
 #app {
@@ -13,5 +37,25 @@
   text-align: center;
   color: #2c3e50;
   font-size: 12px;
+}
+.slide-leave-enter-active,
+.slide-leave-leave-active,
+.slide-enter-enter-active,
+.slide-enter-leave-active {
+  will-change: transform;
+  transition: all 1s;
+  position: absolute;
+}
+.slide-enter-enter {
+  opacity: 1;
+}
+.slide-enter-leave-active {
+  opacity: 0;
+}
+.slide-leave-enter {
+  opacity: 1;
+}
+.slide-leave-leave-active {
+  opacity: 0;
 }
 </style>
