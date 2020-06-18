d1be9b3b546f9d42061f24a95f34fa31b07c16f2
chj <470945645@qq.com>
2020-06-18 14:05:31 +0800

    初始化

--- blog_pre/src/views/Welcome.vue
+++ blog_pre/src/views/Welcome.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="Home">
-    <h1>主页</h1>
+  <div class="Welcome">
+    <h1>欢迎页面</h1>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home',
+  name: 'Welcome',
   components: {
     
   }
